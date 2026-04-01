4c76a75e05770f2a0c1bab2a4f76783800139b82
matias-ort <matias.duro@ort.edu.ar>
2026-03-31 21:33:16 -0300

    clase 3

---
+++ Clase-3/ejercicios/ejercicio3.js
@@ -0,0 +1,57 @@
+const pedidos = [
+  {
+    id: 1,
+    cliente: "Ana García",
+    estado: "entregado",
+    productos: [
+      { nombre: "Teclado", precio: 45, cantidad: 1 },
+      { nombre: "Mouse", precio: 20, cantidad: 2 },
+    ]
+  },
+  {
+    id: 2,
+    cliente: "Luis Pérez",
+    estado: "pendiente",
+    productos: [
+      { nombre: "Monitor", precio: 300, cantidad: 1 },
+      { nombre: "Cable HDMI", precio: 12, cantidad: 3 },
+    ]
+  },
+  {
+    id: 3,
+    cliente: "María López",
+    estado: "cancelado",
+    productos: [
+      { nombre: "Auriculares", precio: 80, cantidad: 1 },
+    ]
+  },
+  {
+    id: 4,
+    cliente: "Carlos Ruiz",
+    estado: "entregado",
+    productos: [
+      { nombre: "Webcam", precio: 65, cantidad: 1 },
+      { nombre: "Mouse", precio: 20, cantidad: 1 },
+      { nombre: "Teclado", precio: 45, cantidad: 2 },
+    ]
+  },
+  {
+    id: 5,
+    cliente: "Sofía Mora",
+    estado: "pendiente",
+    productos: [
+      { nombre: "Monitor", precio: 300, cantidad: 2 },
+    ]
+  },
+]
+
+
+// creen una funcion, para procesar los pedidos
+{
+    totalRecaudado,
+    pendientesDePago,
+    pedidosCancelados,
+    productoMasCaro
+}
+
+//
